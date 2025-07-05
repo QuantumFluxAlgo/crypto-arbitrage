d3a05e775fee662b8696d4b0e32f5a0f7db35bf0
Sean Keane <seankeane785@gmail.com>
2025-07-05 12:04:12 +0100

    feat: add Sentry integration to dashboard
    Added Sentry initialization in the React entry file so errors can be reported and traced, using the VITE_SENTRY_DSN environment variable
    
    Included @sentry/react and @sentry/tracing as dependencies in the dashboard package configuration

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 import './index.css'
 import App from './App.jsx'
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <App />
+    </Sentry.ErrorBoundary>
   </StrictMode>,
 )
