94fa8142e8f2dc6601bb80b2e8be7f7dff26e60e
Sean Keane <seankeane785@gmail.com>
2025-07-14 00:55:25 +0100

    Update run-local script to run jest per project

--- test/run-local.sh
+++ test/run-local.sh
@@ -2,6 +2,7 @@
 # @dev-note: runs mocked tests only
 set -euo pipefail
 export TEST_ENV=local
-npx jest --runInBand
+npx jest --runInBand --config dashboard/jest.config.js
+npx jest --runInBand --config api/jest.config.js
 pytest -m "env('local')"
 ./gradlew test -PtestEnv=local
