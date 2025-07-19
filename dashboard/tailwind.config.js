313b38b8def3b5fae57128ce8bbfd80f832e7375
Sean Keane <seankeane785@gmail.com>
2025-07-19 15:25:07 +0100

    fix: config updates for local load

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-      extend: {
-        colors: {
-          background: "#0B3948",
-          surface: "#D0CDD7",
-          text: "#ACB0BD",
-          primary: "#416165",
-          success: "#00C8A0",
-          error: "#FF5C5C",
-        },
+    extend: {
+      colors: {
+        background: "#0B3948",
+        surface: "#D0CDD7",
+        text: "#ACB0BD",
+        primary: "#416165",
+        success: "#00C8A0",
+        error: "#FF5C5C",
       },
+    },
   },
   plugins: [],
 };
+
